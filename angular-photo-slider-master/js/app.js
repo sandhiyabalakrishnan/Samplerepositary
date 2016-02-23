7a232e58b47c265a959cf139a035a4c29be09d60
sandhiyabalakrishnan <sandhiyabalakrishnan6@gmail.com>
2016-02-23 12:44:23 +0530

    hi slider

---
+++ angular-photo-slider-master/js/app.js
@@ -0,0 +1,84 @@
+angular.module('website', ['ngAnimate', 'ngTouch'])
+    .controller('MainCtrl', function ($scope) {
+        $scope.slides = 
+        [
+        {img: 'images/dish1.jpg',
+        img1: 'images/dish8.jpg',
+        
+
+    
+    },
+
+
+        {img: 'images/dish2.jpg',
+       img1: 'images/dish6.jpg',
+
+    
+    
+},
+  {img: 'images/dish3.jpg',
+  img1: 'images/dish7.jpg',
+  
+    
+},
+       
+    ];
+
+        $scope.direction = 'left';
+        $scope.currentIndex = 0;
+
+        $scope.setCurrentSlideIndex = function (index) {
+            $scope.direction = (index > $scope.currentIndex) ? 'left' : 'right';
+            $scope.currentIndex = index;
+        };
+
+        $scope.isCurrentSlideIndex = function (index) {
+            return $scope.currentIndex === index;
+        };
+
+        $scope.prevSlide = function () {
+            $scope.direction = 'left';
+            $scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? ++$scope.currentIndex : 0;
+        };
+
+        $scope.nextSlide = function () {
+            $scope.direction = 'right';
+            $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slides.length - 1;
+        };
+    })
+    .animation('.slide-animation', function () {
+        return {
+            beforeAddClass: function (element, className, done) {
+                var scope = element.scope();
+
+                if (className == 'ng-hide') {
+                    var finishPoint = element.parent().width();
+                    if(scope.direction !== 'right') {
+                        finishPoint = -finishPoint;
+                    }
+                    TweenMax.to(element, 0.5, {left: finishPoint, onComplete: done });
+                }
+                else {
+                    done();
+                }
+            },
+            removeClass: function (element, className, done) {
+                var scope = element.scope();
+
+                if (className == 'ng-hide') {
+                    element.removeClass('ng-hide');
+
+                    var startPoint = element.parent().width();
+                    if(scope.direction === 'right') {
+                        startPoint = -startPoint;
+                    }
+
+                    TweenMax.fromTo(element, 0.5, { left: startPoint }, {left: 0, onComplete: done });
+                }
+                else {
+                    done();
+                }
+            }
+        };
+    });
+
